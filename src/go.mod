3acf4ba16b838595b269a85ba77e96f01fad8db8
Brian Denicola <brian@briandenicola.com>
2021-06-16 10:04:59 -0500

    Initial Commit

---
+++ src/go.mod
@@ -0,0 +1,8 @@
+module github.com/bjd145/customhandler
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.7.0
+)
---
+++ src/server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"time"
+	"os"
+	"net/http"
+	"fmt"
+	"log"
+	"runtime"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+var version string = "v2"
+
+type OS struct {
+	Time string
+	Host string
+	OSType string
+	Version string
+}
+
+type newAPIHandler struct { }
+func (eh *newAPIHandler) getOperatingSystemHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	
+	host, _ := os.Hostname()
+	ostype := runtime.GOOS 
+	
+	msg := OS{ 
+		time.Now().Format(time.RFC850), 
+		host, 
+		ostype,
+		version}	
+
+	json.NewEncoder(w).Encode(msg)
+}
+
+func (eh *newAPIHandler) optionsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+}
+
+func main() {
+	handler := newAPIHandler{}
+
+	r := mux.NewRouter()
+	apirouter := r.PathPrefix("/api").Subrouter()
+	apirouter.Methods("GET").Path("/os").HandlerFunc(handler.getOperatingSystemHandler)
+	apirouter.Methods("OPTIONS").Path("/os").HandlerFunc(handler.optionsHandler)
+
+	server := cors.Default().Handler(r)
+	
+	//Azure Functions sets FUNCTIONS_CUSTOMHANDLER_PORT to a integer value.  Must append :
+    listenAddr := ":8080"
+    if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+        listenAddr = ":" + val
+    }
+
+	fmt.Print("Listening on ", listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, server))
+}
